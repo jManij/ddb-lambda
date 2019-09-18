a0c5b22cbb50743e9cbf22d3f48d47c8fbc6dba0
Manish KC <kcmanish89@gmail.com>
2019-09-18 13:47:01 -0700

    lambda update

--- src/main/java/ddb/lambda/updateController.java
+++ src/main/java/ddb/lambda/updateController.java
@@ -6,13 +6,12 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.*;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import ddb.lambda.Models.History;
 import ddb.lambda.Models.Task;
 
 import java.util.ArrayList;
+
 
 public class updateController {
 
@@ -21,34 +20,31 @@ public class updateController {
     private Regions REGION = Regions.US_WEST_2;
 
     public Task saveNewTask(Task task, Context context) {
-
-//        DynamoDB db = new DynamoDB(task.getId());
-
-        ArrayList<History> historyArrayList = new ArrayList<>();
-        task.setHistoryArrayList(historyArrayList);
-        task.setNewHistory(new History("Task: " + task.getDescription() + " was added"));
+        /**
+         * For existent tasks
+         */
         final AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.defaultClient();
         DynamoDBMapper ddbMapper = new DynamoDBMapper(ddb);
+        if(task.getId() != null) {
+            Task oldTask = ddbMapper.load(Task.class, task.getId());
+            ArrayList<History> historyArrayList = oldTask.getHistoryArrayList();
+            historyArrayList.add(new History("Task was updated"));
+            task.setHistoryArrayList(historyArrayList);
 
+            /**
+             * Copy the task to old Task
+             */
+            oldTask = task;
+            ddbMapper.save(oldTask);
 
-        ddbMapper.save(task);
-
-
-
+        } else {
+            ArrayList<History> historyArrayList = new ArrayList<>();
+            task.setHistoryArrayList(historyArrayList);
+            task.setNewHistory(new History("Task: " + task.getDescription() + " was added"));
+            Task t = ddbMapper.load(Task.class, task.getId());
+            ddbMapper.save(task);
+        }
         return task;
     }
-//
-//    public Task updateTask(Long Id, Context context) {
-//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-//        DynamoDB dynamoDB = new DynamoDB(client);
-//
-//        Table table = dynamoDB.getTable("task");
-//        Item task = table.getItem("Id", Id);
-//
-//        return task;
-    }
-
-
-
 
 }
